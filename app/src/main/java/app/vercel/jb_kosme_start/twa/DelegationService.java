bd15b5014a6568f1a11cdbfb6330403bdd1639cf
public-afant <public.afant@gmail.com>
2025-08-14 03:29:20 +0900

    fix

---
+++ app/src/main/java/app/vercel/jb_kosme_start/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.jb_kosme_start.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
